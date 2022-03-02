c1d566b4a59dc82acbf4d3439975dd7ed432d186
minkj1992 <minkj1992@gmail.com>
2022-03-02 20:47:00 +0900

    회사노트북 반납용

---
+++ src/app/db.py
@@ -0,0 +1,12 @@
+import os
+
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# SQLAlchemy
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+database = Database(DATABASE_URL)
